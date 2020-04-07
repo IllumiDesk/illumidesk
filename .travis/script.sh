a9d9f3c3dbccdb25105b7f3f9c455f70482550f0
Greg Werner <werner.greg@gmail.com>
2020-04-07 12:32:37 -0400

    chore: Create and upload coverage report (#2)
    * create coverage report with pytest-cov plugin
    * upload to codecov
    * add readme badge

--- .travis/script.sh
+++ .travis/script.sh
@@ -13,13 +13,18 @@ run_unit_tests () {
   python3 -m pytest
 }
 
+run_coverage_report () {
+  echo "Running coverage report ..."
+  pytest --cov=illumidesk tests/
+}
+
 main () {
     # linting errors shouldn't make travis fail
     set +e
     run_linters
     set -e
-    echo "Running unit tests..."
     run_unit_tests
+    run_coverage_report
 }
 
 main
