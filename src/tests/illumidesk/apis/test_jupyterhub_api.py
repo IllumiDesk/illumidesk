debdba530364611417fb636adc060a9fe5df38bd
Ernesto Cruz <ernesto.cruzoviedo@gmail.com>
2020-05-26 23:01:49 -0500

    Add more tests for jupyterhub_api

--- src/tests/illumidesk/apis/test_jupyterhub_api.py
+++ src/tests/illumidesk/apis/test_jupyterhub_api.py
@@ -1,6 +1,10 @@
+import json
 import pytest
 import os
 import uuid
+
+from unittest.mock import patch
+
 from illumidesk.apis.jupyterhub_api import JupyterHubAPI
 from tests.illumidesk.conftest import jupyterhub_api_environ
 
@@ -57,3 +61,42 @@ async def test_create_group_raises_error_with_group_empty(jupyterhub_api_environ
     """
     with pytest.raises(ValueError):
         await JupyterHubAPI().create_group('')
+
+async def request_async_result(endpoint, **kwargs):
+    return True
+
+@pytest.mark.asyncio
+@patch('illumidesk.apis.jupyterhub_api.JupyterHubAPI._request')
+async def test_create_group_uses_request_helper_method_with_correct_values(mock_request, jupyterhub_api_environ):
+    """
+    Does create_group method use the helper method and pass the correct values?
+    """
+    sut = JupyterHubAPI()
+    await sut.create_group('some-value')
+    assert mock_request.called
+    mock_request.assert_called_with('groups/some-value', body='', method='POST')
+
+@pytest.mark.asyncio
+@patch('illumidesk.apis.jupyterhub_api.JupyterHubAPI._request')
+async def test_get_group_uses_request_helper_method_with_correct_values(mock_request, jupyterhub_api_environ):
+    """
+    Does get_group method use the helper method and pass the correct values?
+    """
+    sut = JupyterHubAPI()
+    await sut.get_group('some-value')
+    assert mock_request.called
+    mock_request.assert_called_with('groups/some-value')
+
+@pytest.mark.asyncio
+@patch('illumidesk.apis.jupyterhub_api.JupyterHubAPI._request')
+async def test_create_users_uses_request_helper_method_with_correct_values(mock_request, jupyterhub_api_environ):
+    """
+    Does get_group method use the helper method and pass the correct values?
+    """
+    sut = JupyterHubAPI()
+    await sut.create_users('new_user')
+    assert mock_request.called
+    body_usernames ={
+        'usernames': ['new_user']
+    }
+    mock_request.assert_called_with('users', method='POST', body=json.dumps(body_usernames))
