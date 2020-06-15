211a8205cead1b1fe1671ac420f9015802cd823e
Greg <werner.greg@gmail.com>
2020-06-15 12:35:15 -0400

    fix merge conflicts
    Signed-off-by: Greg <werner.greg@gmail.com>

--- src/illumidesk/authenticators/handlers.py
+++ src/illumidesk/authenticators/handlers.py
@@ -14,7 +14,11 @@ from tornado.web import RequestHandler
 from tornado import web
 from tornado.web import HTTPError
 
+<<<<<<< HEAD
 from typing import cast
+=======
+# from tornado.web import RequestHandler
+>>>>>>> 9cb6ef6... fix merge conflicts
 
 from illumidesk.authenticators.validator import LTI13LaunchValidator
 from illumidesk.authenticators.utils import LTIUtils
@@ -127,6 +131,7 @@ class LTI13LoginHandler(OAuthLoginHandler):
 
     def post(self):
         """
+<<<<<<< HEAD
         Validates required login arguments sent from platform and then uses
         the ``authorize_redirect()`` to redirect users to the authorization url.
         """
@@ -156,6 +161,86 @@ class LTI13LoginHandler(OAuthLoginHandler):
                 redirect_uri=redirect_uri,
                 state=state,
             )
+=======
+        Handles LTI 1.3 authentication by building a dict with the required values
+        and then redirecting the user to the callback URL using the redirect_uri
+        that in turn uses the guess_callback_uri function from the oauthenticator.oauth2
+        module.
+        """
+        login_hint = self.get_argument('login_hint')
+        self.log.debug('Received login_hint: %s' % login_hint)
+        lti_message_hint = self.get_argument('lti_message_hint')
+        self.log.debug('Received lti_message_hint is %s' % lti_message_hint)
+        client_id = self.get_argument('client_id')
+        self.log.debug('Received client_id is %s' % client_id)
+        nonce = self.get_argument('nonce')
+        state = self.get_state()
+        self.set_state_cookie(state)
+        self.log.debug('State cookie set to %s' % state)
+        redirect_uri = self.get_argument('redirect_uri')
+        self.log.debug('Received redirect_uri: %s' % redirect_uri)
+        self.authorize_redirect(
+            client_id, lti_message_hint, login_hint, nonce, redirect_uri, state,
+        )
+
+    def authorize_redirect(
+        self,
+        prompt: str = 'none',
+        response_mode: str = 'form_post',
+        response_type: str = 'id_token',
+        scope: str = 'openid',
+        client_id: str = None,
+        lti_message_hint: str = None,
+        login_hint: str = None,
+        nonce: str = None,
+        redirect_uri: str = None,
+        state: str = None,
+    ) -> None:
+        """
+        Implements the OAuth2Mixin to to initiate the LTI 1.3 / OIDC login flow.
+        Arguments are sent to the platform's authorization server for further processing.
+
+        References:
+        https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest
+        http://www.imsglobal.org/spec/lti/v1p3/#additional-login-parameters-0
+        
+        Args:
+          prompt: used to prompt user for reauthentication and consent, defaults to none
+          response_mode: how the platform responds to the tool, defaults to form_post
+          scope: oauth2 scope, defaults to 'openid'
+          response_type: oauth2 response type, defaults to 'id_token'
+          client_id: used to identify the tool's installation in a platform
+          redirect_uri: endpoint specified during tool installation (callback url)
+          extra_params: dictionary that contains extra parameters for oauth redirect
+          
+        """
+        # handler = cast(RequestHandler, self)
+        # use the first argument to set up dictionary
+        args = {'prompt': prompt}
+        args['response_mode'] = response_mode
+        args['response_type'] = response_type
+        args['scope'] = scope
+        if not client_id:
+            raise HTTPError(403, 'Missing client_id')
+        args['client_id'] = self.get_argument('client_id')
+        if not lti_message_hint:
+            raise HTTPError(403, 'Missing lti_message_hint')
+        args['lti_message_hint'] = self.get_argument('lti_message_hint')
+        if not login_hint:
+            raise HTTPError(403, 'Missing login_hint')
+        args['login_hint'] = self.get_argument('login_hint')
+        if not nonce:
+            raise HTTPError(403, 'Missing nonce')
+        args['nonce'] = self.get_argument('nonce')
+        if not redirect_uri:
+            raise HTTPError(403, 'Missing redirect_uri')
+        args['redirect_uri'] = self.get_argument('redirect_uri')
+        if not state:
+            raise HTTPError(403, 'Missing redirect_uri')
+        args['state'] = self.get_argument('state')
+        url = self._OAUTH_AUTHORIZE_URL  # type: ignore
+        # handler.redirect(url_concat(url, args))
+>>>>>>> 9cb6ef6... fix merge conflicts
 
 
 class LTI13CallbackHandler(OAuthCallbackHandler):
