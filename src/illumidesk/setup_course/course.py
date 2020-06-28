abbd847ea44c736a76e9f3259c76e42c96ae95be
Greg <werner.greg@gmail.com>
2020-06-28 17:15:30 -0400

    minor extra line update
    Signed-off-by: Greg <werner.greg@gmail.com>

--- src/illumidesk/setup_course/course.py
+++ src/illumidesk/setup_course/course.py
@@ -40,7 +40,6 @@ class Course:
         self.domain = domain
         self.jupyterhub_api_url = os.environ.get('JUPYTERHUB_API_URL')
         self.jupyterhub_base_url = os.environ.get('JUPYTERHUB_BASE_URL')
-
         self.exchange_root = Path(os.environ.get('MNT_ROOT'), self.org, 'exchange')
         self.grader_name = f'grader-{course_id}'
         self.grader_root = Path(os.environ.get('MNT_ROOT'), org, 'home', self.grader_name,)
@@ -170,7 +169,6 @@ class Course:
                 f'JUPYTERHUB_SERVICE_NAME={self.course_id}',
                 f'JUPYTERHUB_API_TOKEN={self.token}',
                 f'JUPYTERHUB_API_URL={self.jupyterhub_api_url}',
-                f'JUPYTERHUB_BASE_URL={self.jupyterhub_base_url}',
                 f'JUPYTERHUB_SERVICE_PREFIX=/services/{self.course_id}',
                 f'JUPYTERHUB_CLIENT_ID=service-{self.course_id}',
                 f'JUPYTERHUB_USER={self.grader_name}',
