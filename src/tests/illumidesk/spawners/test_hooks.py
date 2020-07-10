75aaa2a1a395d7f4d0f7c8b818cb57331ace75f6
Ernesto Cruz <ernesto.cruzoviedo@gmail.com>
2020-07-09 20:23:19 -0500

    fix hooks unittests

--- src/tests/illumidesk/spawners/test_hooks.py
+++ src/tests/illumidesk/spawners/test_hooks.py
@@ -9,7 +9,7 @@ async def test_ensure_environment_assigned_to_user_role_from_auth_state_in_spawn
     Does the user's docker container environment reflect his/her role?
     """
     sut = IllumiDeskRoleDockerSpawner()
-    await custom_auth_state_hook(sut, auth_state_dict['auth_state'])
+    custom_auth_state_hook(sut, auth_state_dict['auth_state'])
     # make sure the hook set the environment variables
     assert sut.environment['USER_ROLE'] == 'Learner'
 
@@ -20,7 +20,7 @@ async def test_ensure_environment_assigned_to_workspace_type_from_auth_state_in_
     Does the user's docker container environment reflect his/her role?
     """
     sut = IllumiDeskRoleDockerSpawner()
-    await custom_auth_state_hook(sut, auth_state_dict['auth_state'])
+    custom_auth_state_hook(sut, auth_state_dict['auth_state'])
     # make sure the hook set the environment variables
     assert sut.environment['USER_WORKSPACE_TYPE'] == 'notebook'
 
@@ -33,7 +33,7 @@ async def test_auth_state_hook_does_not_raise_an_error_if_workspace_type_is_miss
     sut = IllumiDeskRoleDockerSpawner()
     # remove the workspace_type key from auth_state
     del auth_state_dict['auth_state']['workspace_type']
-    await custom_auth_state_hook(sut, auth_state_dict['auth_state'])
+    custom_auth_state_hook(sut, auth_state_dict['auth_state'])
     # make sure the hook set the environment
     assert sut.environment
     assert 'USER_WORKSPACE_TYPE' not in sut.environment
