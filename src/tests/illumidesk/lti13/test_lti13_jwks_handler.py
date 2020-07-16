b7cce0e0447bb285a4f161c44edba72df59127df
Ernesto Cruz <ernesto.cruzoviedo@gmail.com>
2020-07-16 13:22:39 -0500

    Add unitests to check the content-type as json

--- src/tests/illumidesk/lti13/test_lti13_jwks_handler.py
+++ src/tests/illumidesk/lti13/test_lti13_jwks_handler.py
@@ -1,10 +1,13 @@
 from os import chmod
 from os import environ
+
 import pytest
 
 from illumidesk.lti13.handlers import LTI13JWKSHandler
 
 from tornado.web import RequestHandler
+
+from unittest.mock import patch
 
 
 @pytest.mark.asyncio
@@ -34,3 +37,31 @@ async def test_get_method_raises_an_error_without_lti13_private_key(make_mock_re
     config_handler = LTI13JWKSHandler(handler.application, handler.request)
     with pytest.raises(EnvironmentError):
         await config_handler.get()
+
+
+@patch('tornado.web.RequestHandler.write')
+def test_get_method_calls_write_method_with_a_dict(mock_write_method, lti_config_environ, make_mock_request_handler):
+    """
+    Does the write method is called with a dict?
+    """
+    handler = make_mock_request_handler(RequestHandler)
+    config_handler = LTI13JWKSHandler(handler.application, handler.request)
+
+    config_handler.get()
+    assert mock_write_method.called
+    write_args = mock_write_method.call_args[0]
+    # make sure we're passing a dict to let tornado convert it as json with the specific content-type
+    assert write_args[0]
+    assert type(write_args[0]) == dict
+
+
+def test_get_method_set_content_type_as_json(lti_config_environ, make_mock_request_handler):
+    """
+    Does the write method is set the content-type header as application/json?
+    """
+    handler = make_mock_request_handler(RequestHandler)
+    config_handler = LTI13JWKSHandler(handler.application, handler.request)
+
+    config_handler.get()
+    assert 'Content-Type' in config_handler._headers
+    assert 'application/json' in config_handler._headers['Content-type']
