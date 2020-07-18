57f53619ea8b6ea8dac09d19d9f0dbcf0f173e69
Ernesto <ernesto.cruzoviedo@gmail.com>
2020-07-17 19:02:53 -0500

    fix: Update nbgrader config template by adding a escape characters (#230)

--- src/illumidesk/setup_course/constants.py
+++ src/illumidesk/setup_course/constants.py
@@ -4,8 +4,8 @@ c = get_config()
 c.CourseDirectory.root = '/home/{grader_name}/{course_id}'
 c.CourseDirectory.course_id = '{course_id}'
 c.ClearSolutions.code_stub = {{
-    'python': '# your code here\nraise NotImplementedError',
-    'javascript': '// your code here\nthrow new Error();',
-    'julia': '# your code here\nthrow(ErrorException())'
+    "python": "# your code here\\nraise NotImplementedError",
+    "javascript": "// your code here\\nthrow new Error();",
+    "julia": "# your code here\\nthrow(ErrorException())"
 }}
 """
