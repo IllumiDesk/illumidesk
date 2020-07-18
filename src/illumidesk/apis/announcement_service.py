b06370e16cc99bc45b6221a42e0b171e193e021e
Ernesto Cruz <ernesto.cruzoviedo@gmail.com>
2020-07-18 17:15:52 -0500

    Add AnnouncementService class to manage the announcements to the user

--- src/illumidesk/apis/announcement_service.py
+++ src/illumidesk/apis/announcement_service.py
@@ -1,8 +1,35 @@
+import json
 import os
+from tornado.httpclient import AsyncHTTPClient
 
-
-ANNOUNCEMENT_PORT = os.environ.get('ANNOUNCEMENT_SERVICE_PORT') or '8889'
 jhub_base_url = os.environ.get('JUPYTERHUB_BASE_URL') or ''
+# Constants
+ANNOUNCEMENT_PORT = os.environ.get('ANNOUNCEMENT_SERVICE_PORT') or '8889'
 ANNOUNCEMENT_PREFIX = f'{jhub_base_url}/services/announcement'
 
 ANNOUNCEMENT_INTERNAL_URL = f'http://localhost:{int(ANNOUNCEMENT_PORT)}{ANNOUNCEMENT_PREFIX}'
+
+ANNOUNCEMENT_JHUB_SERVICE_DEFINITION = {
+    'name': 'announcement',
+    'url': f'http://0.0.0.0:{int(ANNOUNCEMENT_PORT)}',  # allow external connections with 0.0.0.0
+    'command': f'python3 /srv/jupyterhub/announcement.py --port {int(ANNOUNCEMENT_PORT)} --api-prefix {ANNOUNCEMENT_PREFIX}'.split(),
+}
+
+
+class AnnouncementService:
+    """
+    Class helper to add announcements using a jupyterhub internal service (normally located at services/announcement)
+    """
+    @classmethod
+    async def add_announcement(cls, message: str) -> None:
+        # It should not raise an error if a message is not passed
+        if not message:
+            return
+        # we need call jhub services with Authorization header
+        jupyterhub_api_token = os.environ.get('JUPYTERHUB_API_TOKEN')
+        
+        headers = {'Content-Type': 'application/json'}
+        headers['Authorization'] = f'token {jupyterhub_api_token}'
+        body_data = {'announcement': message}
+        client = AsyncHTTPClient()
+        await client.fetch(ANNOUNCEMENT_INTERNAL_URL, headers=headers, body=json.dumps(body_data), method='POST')
---
+++ src/tests/illumidesk/apis/test_announcement_service.py
@@ -0,0 +1,26 @@
+import json
+import os
+import pytest
+
+from illumidesk.apis.announcement_service import ANNOUNCEMENT_INTERNAL_URL, AnnouncementService
+
+from tornado.httpclient import AsyncHTTPClient
+from tornado.web import RequestHandler
+
+from unittest.mock import patch
+
+
+@pytest.mark.asyncio
+async def test_add_announcement__call_fetch_with_correct_url(jupyterhub_api_environ, make_http_response, make_mock_request_handler):
+    local_handler = make_mock_request_handler(RequestHandler)
+    sut = AnnouncementService
+    headers = {'Content-Type': 'application/json', 'Authorization': f'token {os.environ.get("JUPYTERHUB_API_TOKEN")}'}
+    with patch.object(AsyncHTTPClient, 'fetch', return_value=make_http_response(handler=local_handler.request)) as mock_client:
+        await sut.add_announcement('A new service was detected, please reload this page...')
+        # the announcement service was called by using specific args
+        mock_client.assert_any_call(
+            ANNOUNCEMENT_INTERNAL_URL,
+            headers=headers,
+            body=json.dumps({'announcement': 'A new service was detected, please reload this page...'}),
+            method='POST',
+        )
