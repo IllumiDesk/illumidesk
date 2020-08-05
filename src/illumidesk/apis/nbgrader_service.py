aaf89f27c8154cc3beefd5760063997e30f6e618
Ernesto <ernesto.cruzoviedo@gmail.com>
2020-08-04 21:07:20 -0500

    fix: Normalize the assignment name and course-id (#264)

--- src/illumidesk/apis/nbgrader_service.py
+++ src/illumidesk/apis/nbgrader_service.py
@@ -2,36 +2,42 @@ import logging
 from pathlib import Path
 import os
 import shutil
-import sys
 
 from nbgrader.api import Assignment, Course, Gradebook
 from nbgrader.api import InvalidEntry
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+from illumidesk.authenticators.utils import LTIUtils
+
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class NbGraderServiceHelper:
+    """
+    Helper class to facilitate the use of nbgrader database and its methods
+    """
+
     def __init__(self, course_id: str):
         if not course_id:
             raise ValueError('course_id missing')
-        self.course_id = course_id
+
+        self.course_id = LTIUtils().normalize_string(course_id)
         self.uid = int(os.environ.get('NB_UID') or '10001')
         self.gid = int(os.environ.get('NB_GID') or '100')
-        grader_name = f'grader-{course_id}'
-        self.course_dir = f'/home/{grader_name}/{course_id}'
-
+        grader_name = f'grader-{self.course_id}'
+        self.course_dir = f'/home/{grader_name}/{self.course_id}'
         self.gradebook_path = Path(self.course_dir, 'gradebook.db')
         # make sure the gradebook path exists
         self.gradebook_path.parent.mkdir(exist_ok=True, parents=True)
         logger.debug('Gradebook path is %s' % self.gradebook_path)
-        logger.debug("creating gradebook instance")
-        self.gb = Gradebook(f'sqlite:///{self.gradebook_path}', course_id=self.course_id)
-        logger.debug(
-            'Changing or making sure the gradebook directory permissions (with path %s) to %s:%s '
-            % (self.gradebook_path, self.uid, self.gid)
-        )
-        shutil.chown(str(self.gradebook_path), user=self.uid, group=self.gid)
+        logger.debug("Creating gradebook instance")
+        # With new Gradebook instance the database is initiated/created
+        with Gradebook(f'sqlite:///{self.gradebook_path}', course_id=self.course_id):
+            logger.debug(
+                'Changing or making sure the gradebook directory permissions (with path %s) to %s:%s '
+                % (self.gradebook_path, self.uid, self.gid)
+            )
+            shutil.chown(str(self.gradebook_path), user=self.uid, group=self.gid)
 
     def update_course(self, **kwargs) -> None:
         with Gradebook(f'sqlite:///{self.gradebook_path}', course_id=self.course_id) as gb:
@@ -54,6 +60,8 @@ class NbGraderServiceHelper:
         """
         if not assignment_name:
             raise ValueError('assignment_name missing')
+        assignment_name = LTIUtils().normalize_string(assignment_name)
+        logger.debug('Assignment name normalized %s to save in gradebook' % assignment_name)
         assignment = None
         with Gradebook(f'sqlite:///{self.gradebook_path}', course_id=self.course_id) as gb:
             try:
---
+++ src/tests/illumidesk/apis/test_nbgrader_service_helper.py
@@ -0,0 +1,59 @@
+import pytest
+
+from unittest.mock import patch
+
+from illumidesk.apis.nbgrader_service import NbGraderServiceHelper
+
+
+def test_course_id_required_otherwise_raises_an_error():
+    with pytest.raises(ValueError):
+        NbGraderServiceHelper('')
+
+
+@patch('shutil.chown')
+@patch('pathlib.Path.mkdir')
+@patch('illumidesk.apis.nbgrader_service.Gradebook')
+def test_course_id_is_normalized_in_the_constructor(mock_gradebook, mock_path_mkdir, mock_chown):
+    sut = NbGraderServiceHelper('PS- ONE')
+    assert sut.course_id == 'ps-one'
+
+
+@patch('shutil.chown')
+@patch('pathlib.Path.mkdir')
+@patch('illumidesk.apis.nbgrader_service.Gradebook')
+def test_graderbook_is_formed_correctly(mock_gradebook, mock_path_mkdir, mock_chown):
+    course_id = 'PS- ONE 123'
+    sut = NbGraderServiceHelper(course_id)
+    assert str(sut.gradebook_path) == '/home/grader-ps-one123/ps-one123/gradebook.db'
+
+
+@patch('shutil.chown')
+@patch('pathlib.Path.mkdir')
+@patch('illumidesk.apis.nbgrader_service.Gradebook')
+def test_chown_is_called_to_fix_permissions(mock_gradebook, mock_path_mkdir, mock_chown):
+    course_id = 'PS- ONE 123'
+    NbGraderServiceHelper(course_id)
+    assert mock_chown.called
+
+
+@patch('shutil.chown')
+@patch('pathlib.Path.mkdir')
+@patch('illumidesk.apis.nbgrader_service.Gradebook')
+def test_mkdir_is_called_with_grader_home_path(mock_gradebook, mock_path_mkdir, mock_chown):
+    course_id = 'PS- ONE 123'
+    NbGraderServiceHelper(course_id)
+    assert mock_path_mkdir.called
+
+
+@patch('os.makedirs')
+@patch('shutil.chown')
+@patch('pathlib.Path.mkdir')
+@patch('illumidesk.apis.nbgrader_service.Gradebook')
+def test_create_assignment_in_nbgrader_uses_the_assignment_name_normalized(
+    mock_gradebook, mock_path_mkdir, mock_chown, mock_makedirs
+):
+    course_id = 'PS- ONE 123'
+    sut = NbGraderServiceHelper(course_id)
+    sut.create_assignment_in_nbgrader('LAB 1')
+    assert mock_gradebook.return_value.__enter__.return_value.update_or_create_assignment.called
+    assert mock_gradebook.return_value.__enter__.return_value.update_or_create_assignment.call_args[0][0] == 'lab1'
