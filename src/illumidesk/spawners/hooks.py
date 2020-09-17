37173b6824c4d9ef8bcae50ae16982dfd4223b7f
Ernesto <ernesto.cruzoviedo@gmail.com>
2020-09-16 21:42:22 -0500

    feat: Shares folders and files only for courses the user is enrolled in (#320)

--- src/illumidesk/spawners/hooks.py
+++ src/illumidesk/spawners/hooks.py
@@ -3,18 +3,40 @@ import shutil
 
 from jupyterhub.spawner import Spawner
 
+from illumidesk.authenticators.utils import user_is_an_instructor
+
 
 def custom_auth_state_hook(spawner: Spawner, auth_state: dict) -> None:
     """
-    Customized hook to assign USER_ROLE environment variable to LTI user role.
-    The USER_ROLE environment variable is used to select the notebook image based
-    on the user's role.
+    Customized hook to:
+    - set environment variables, for example the USER_ROLE from LTI user role.
+    - obtain the course_id from auth_state to add the shared folder in the volumes list dynamically
     """
     if not auth_state:
         raise ValueError('auth_state not enabled.')
     spawner.log.debug('auth_state_hook set with %s role' % auth_state['user_role'])
-    spawner.environment['USER_ROLE'] = auth_state['user_role']
+    user_role = auth_state['user_role']
+    # set spawner environment
+    spawner.environment['USER_ROLE'] = user_role
     spawner.log.debug('Assigned USER_ROLE env var to %s' % spawner.environment['USER_ROLE'])
+    # get the course-id from auth_state to add the shared folder only for this course
+    course_id = auth_state['course_id']
+    # Organization name
+    org_name = os.environ.get('ORGANIZATION_NAME') or 'my-org'
+    # Notebook directory within docker image
+    notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR')
+    # Root directory to mount org, home, and exchange folders
+    mnt_root = os.environ.get('MNT_ROOT')
+    # add the shared folder as a volume if it was enabled
+    shared_folder_enabled = os.environ.get('SHARED_FOLDER_ENABLED') or 'False'
+    # shared-folder feat is enabled but we make sure the instructor must have it
+    shared_folder_allowed = (
+        True if not user_is_an_instructor(user_role) else spawner.load_shared_folder_with_instructor
+    )
+    if shared_folder_enabled.lower() in ('true', '1') and course_id and shared_folder_allowed:
+        spawner.log.debug('Adding the shared folder for %s' % course_id)
+        spawner.volumes[f'{mnt_root}/{org_name}' + '/shared/' + course_id] = notebook_dir + '/shared'
+        spawner.log.debug(f'Volumes to mount {spawner.volumes}')
 
 
 def custom_pre_spawn_hook(spawner: Spawner) -> None:
--- src/tests/illumidesk/spawners/test_illumidesk_dockerspawner.py
+++ src/tests/illumidesk/spawners/test_illumidesk_dockerspawner.py
@@ -2,56 +2,6 @@ import types
 
 from dockerspawner.dockerspawner import DockerSpawner
 
-from illumidesk.spawners.spawners import IllumiDeskDockerSpawner
-
-
-def test_base_spawner_does_not_consider_shared_folder_with_instructor_role_when_jhub_setting_is_False(
-    setup_image_environ, mock_jhub_user
-):
-    """
-    Does the IllumiDeskDockerSpawner class exclude the shared folder for instructors when load_shared_folder_with_instructor is False?
-    """
-    sut = IllumiDeskDockerSpawner(load_shared_folder_with_instructor=False)
-    sut.environment['USER_ROLE'] = 'Instructor'
-    sut.user = mock_jhub_user()
-    volumes_to_mount = {'mnt_root/my-org/shared/': 'home/jovyan/shared'}
-    binds = sut._volumes_to_binds(volumes=volumes_to_mount, binds={})
-    assert len(binds) == 0
-
-
-def test_base_spawner_load_shared_folder_with_instructor_role_when_jhub_setting_is_True(
-    setup_image_environ, mock_jhub_user
-):
-    """
-    Does the IllumiDeskDockerSpawner class load the shared folder for instructors?
-    """
-    sut = IllumiDeskDockerSpawner(load_shared_folder_with_instructor=True)
-    sut.environment['USER_ROLE'] = 'Instructor'
-    sut.user = mock_jhub_user()
-    volumes_to_mount = {'mnt_root/my-org/shared/': 'home/jovyan/shared'}
-    binds = sut._volumes_to_binds(volumes=volumes_to_mount, binds={})
-    assert len(binds) == 1
-
-
-def test_base_spawner_returns_the_shared_folder_with_learner_role_not_matter_the_jhub_setting(
-    setup_image_environ, mock_jhub_user
-):
-    """
-    Does the IllumiDeskDockerSpawner class consider the shared folder for learners?
-    """
-    sut = IllumiDeskDockerSpawner()
-    sut.environment['USER_ROLE'] = 'Learner'
-    sut.user = mock_jhub_user()
-    volumes_to_mount = {'mnt_root/my-org/shared/': 'home/jovyan/shared'}
-    binds = sut._volumes_to_binds(volumes=volumes_to_mount, binds={})
-    # First case (setting is not used)
-    assert len(binds) == 1
-    sut.load_shared_folder_with_instructor = False
-    volumes_to_mount = {'mnt_root/my-org/shared/': 'home/jovyan/shared'}
-    binds = sut._volumes_to_binds(volumes=volumes_to_mount, binds={})
-    # First case (setting is used)
-    assert len(binds) == 1
-
 
 def test_dockerspawner_uses_raw_username_in_format_volume_name():
     """
