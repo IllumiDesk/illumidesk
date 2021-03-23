3a816bfac6c0059215946f0f4aa4f78992402f5f
Greg Werner <werner.greg@gmail.com>
2021-03-22 20:41:29 -0400

    readd _version
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

---
+++ src/grader-service/_version.py
@@ -0,0 +1,13 @@
+"""illumidesk grader-service version info"""
+
+# for now, update the version so that its the same as the one reflected
+# within the repo's root package.json
+version_info = (
+    2,
+    0,
+    0,
+)
+__version__ = ".".join(map(str, version_info[:3]))
+
+if len(version_info) > 3:
+    __version__ = "%s%s" % (__version__, version_info[3])
